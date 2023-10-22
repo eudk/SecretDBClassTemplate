21051cf57e21edcc8dc6c5b57b5cdbc689ac7548
Evgeniy Gordienko <112853162+eudk@users.noreply.github.com>
2023-10-22 19:11:19 +0200

    Add project files.

---
+++ SecretDBClassTemplate/Secret.cs
@@ -0,0 +1,15 @@
+﻿using System;
+//using Microsoft.EntityFrameworkCore.Infrastructure; 
+
+namespace SecretDBClassTemplate
+{
+    public class Secrets
+    {
+        public static string ConnectionString = "Data Source=MS SQL host name;Initial Catalog=dbname;user id=USERNAME;password=PASSWORD;TrustServerCertificate=true";
+    }
+}
+
+// Data Source is the MS SQL host name eg. Simply....
+// Initial Catallog is the Database name
+// user id is the username
+// password is the password
